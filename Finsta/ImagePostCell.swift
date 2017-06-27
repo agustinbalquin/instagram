f4bcc1c3cb4a6f7d55fdabc20eb666981cf908af
agustinbalquin <agustinbalquin@gmail.com>
2017-06-27 03:56:24 -0700

    Added post model

---
+++ Finsta/ImagePostCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImagePostCell.swift
+//  Finsta
+//
+//  Created by Agustin Balquin on 6/27/17.
+//  Copyright © 2017 Agustin Balquin. All rights reserved.
+//
+
+import UIKit
+
+class ImagePostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
