ec56ec47f0f8d0e4741bb39c4e0b7e06b2fa9a28
agustinbalquin <agustinbalquin@gmail.com>
2017-06-27 15:46:24 -0700

    Added Image Posting Viewcontroller

--- Finsta/PhotoMapViewController.swift
+++ Finsta/PhotoMapViewController.swift
@@ -171,19 +171,7 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
     // ================
     
     @IBAction func postPhoto(_ sender: Any) {
-        print("postImage")
-        let vc = UIImagePickerController()
-        vc.delegate = self
-        vc.allowsEditing = true
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            print("Camera is available 📸")
-            vc.sourceType = .camera
-        } else {
-            print("Camera 🚫 available so we will use photo library instead")
-            vc.sourceType = .photoLibrary
-        }
-        
-        self.present(vc, animated: true, completion: nil)
+        performSegue(withIdentifier: "postSegue", sender: nil)
     }
     
     
@@ -196,25 +184,7 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
     
     
     
-    func imagePickerController(_ picker: UIImagePickerController,
-                               didFinishPickingMediaWithInfo info: [String : Any]) {
-        // Get the image captured by the UIImagePickerController
-        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
-        let caption = "Temporary Caption"
-        
-        Post.postUserImage(image: editedImage, withCaption: caption) { (success: Bool,error: Error?) in
-            if success {
-                print("Posted to Instagram!")
-                
-            } else if let error = error {
-                print("Problem posting: \(error.localizedDescription)")
-            }
-        }
-        
-        // Dismiss UIImagePickerController to go back to your original view controller
-        dismiss(animated: true, completion: nil)
-    }
+
     
 
 }
---
+++ Finsta/PostViewController.swift
@@ -0,0 +1,78 @@
+//
+//  PostViewController.swift
+//  Finsta
+//
+//  Created by Agustin Balquin on 6/27/17.
+//  Copyright © 2017 Agustin Balquin. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func setImage(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    @IBAction func sendPost(_ sender: Any) {
+        let caption = captionField.text ?? ""
+        if imageView.image == nil {
+            let alertController = UIAlertController(title: "Post Failed", message: "Image was not selected", preferredStyle: .alert)
+            let OKAction = UIAlertAction(title: "Try Again", style: .default) { (action) in }
+            alertController.addAction(OKAction)
+            self.present(alertController, animated: true) { }
+
+            return
+        }
+        Post.postUserImage(image: imageView.image, withCaption: caption) { (success: Bool,error: Error?) in
+            if success {
+                let alertController = UIAlertController(title: "Success", message: "Image was posted to Instagram", preferredStyle: .alert)
+                
+                // create an OK action
+                let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in }
+                alertController.addAction(OKAction)
+                
+                self.present(alertController, animated: true) { }
+                
+            } else if let error = error {
+                print("Problem posting: \(error.localizedDescription)")
+            }
+        }
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        imageView.image = editedImage
+
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+
+}
