480a0ccc11debd9821ac0cfc4e72c8afb871b4c7
agustinbalquin <agustinbalquin@gmail.com>
2017-06-28 13:16:31 -0700

    Changed info.plist, Added User ViewController, Added Collectionview

---
+++ Finsta/UserCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserCollectionViewCell.swift
+//  Finsta
+//
+//  Created by Agustin Balquin on 6/28/17.
+//  Copyright © 2017 Agustin Balquin. All rights reserved.
+//
+
+import UIKit
+
+class UserCollectionViewCell: UICollectionViewCell {
+    
+}
