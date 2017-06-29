1f1d381ea429e4f08ef135f3ed32f7f3322a6150
agustinbalquin <agustinbalquin@gmail.com>
2017-06-29 13:47:01 -0700

    Added caption cell

---
+++ Finsta/CaptionCell.swift
@@ -0,0 +1,24 @@
+//
+//  CaptionCell.swift
+//  Finsta
+//
+//  Created by Agustin Balquin on 6/29/17.
+//  Copyright © 2017 Agustin Balquin. All rights reserved.
+//
+
+import UIKit
+
+class CaptionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
